26623df6443be4261730a8db225ee030ef59ce57
anchapin <a.n.chapin@gmail.com>
2024-05-21 10:35:36 -0600

    Final version, everything seems to be working other than the hourly profile adding feature

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import os
 from flask import Flask, render_template
 import folium
 from pykml import parser
-import random
 import json
 from shapely.geometry import Point, Polygon
 import pandas as pd
+import random
 
 app = Flask(__name__)
 
@@ -31,9 +31,11 @@ def find_csv_file(property_name, folder_path='hourly_profiles'):
 
 def calculate_peak_demand(csv_file):
     if csv_file:
-        df = pd.read_csv(csv_file)
+        df = pd.read_csv(csv_file, delimiter=';')
         if 'Modified Power Demand (kW)' in df.columns:
             return df['Modified Power Demand (kW)'].max()
+        else:
+            return df['Power Demand (kW)'].max()
     return 0
 
 projects = load_projects_from_geojson('projects.geojson')
@@ -45,27 +47,39 @@ def kml_to_geojson(kml_path):
     legend_info = []
     for placemark in root.findall('.//{http://www.opengis.net/kml/2.2}Placemark'):
         name = placemark.find('{http://www.opengis.net/kml/2.2}name').text
-        geometry = placemark.find('.//{http://www.opengis.net/kml/2.2}Polygon')
-        if geometry is not None:
-            coords = geometry.find('.//{http://www.opengis.net/kml/2.2}coordinates').text.strip()
-            coordinates = [[list(map(float, coord.split(',')))[:2] for coord in coords.split()]]
-            polygon = Polygon(coordinates[0])
-            color = "#{:06x}".format(random.randint(0, 0xFFFFFF))
+        geometry = placemark.find('./{http://www.opengis.net/kml/2.2}Polygon')
+        if geometry:
+            coordinates = geometry.findall('.//{http://www.opengis.net/kml/2.2}coordinates')
+            coords_list = []
+            for coord in coordinates:
+                coord_text = coord.text.strip()
+                coord_pairs = coord_text.split()
+                coords = [(float(pair.split(',')[0]), float(pair.split(',')[1])) for pair in coord_pairs]
+                coords_list.append(coords)
+            polygon = Polygon(coords_list[0])
             peak_demand_sum = 0
             for project in projects:
                 point = Point(project["location"][1], project["location"][0])  # Note: Point takes (longitude, latitude)
                 if polygon.contains(point):
                     peak_demand_sum += calculate_peak_demand(project["csv_file"]) or 0
-            feature = {
+            geojson['features'].append({
                 "type": "Feature",
-                "properties": {"name": name, "color": color, "peak_demand": peak_demand_sum},
+                "properties": {
+                    "name": name,
+                    "color": "#" + ''.join([random.choice('0123456789ABCDEF') for j in range(6)])  # Random color for demo
+                },
                 "geometry": {
                     "type": "Polygon",
-                    "coordinates": coordinates
+                    "coordinates": [coords_list[0]]
                 }
-            }
-            legend_info.append({"name": name, "color": color, "peak_demand": peak_demand_sum})
-            geojson["features"].append(feature)
+            })
+            legend_info.append({
+                "name": name,
+                "color": geojson['features'][-1]['properties']['color'],
+                "peak_demand": peak_demand_sum  # Placeholder for peak demand
+            })
+    # Sort legend_info by peak_demand in descending order, converting to float
+    legend_info.sort(key=lambda x: float(str(x['peak_demand']).replace(',', '')), reverse=True) 
     return geojson, legend_info
 
 @app.route('/')
@@ -90,7 +104,9 @@ def index():
 
     # Add project locations (as example markers)
     for project in projects:
-        folium.Marker(location=project["location"], popup=f'{project["name"]}: Peak demand added= {calculate_peak_demand(project["csv_file"])} kW').add_to(m)
+        peak_demand = calculate_peak_demand(project["csv_file"])
+        formatted_peak_demand = f"{peak_demand:,.0f} kW"
+        folium.Marker(location=project["location"], popup=f'{project["name"]}: Peak demand: {formatted_peak_demand}').add_to(m)
 
     # Create a legend
     legend_html = '''
@@ -101,9 +117,10 @@ def index():
         <div style="background:white; padding: 10px;">
     '''
     for item in legend_info:
+        formatted_peak_demand = f"{float(str(item['peak_demand']).replace(',', '')):,.0f} kW"
         legend_html += f'''
         <i style="background:{item['color']};width:20px;height:20px;float:left;margin-right:8px;"></i>
-        {item['name']} - Peak demand: {item['peak_demand']} kW<br>
+        {item['name']} - Peak demand: {formatted_peak_demand}<br>
         '''
     legend_html += '''
         </div>
